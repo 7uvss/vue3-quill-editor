560385b13a93d32f2069746a9c33a07eb85cc10a
mashihao_i <mashihao_i@didiglobal.com>
2021-06-01 15:06:02 +0800

    fix: change export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import richTextEditor from "./components/richTextEditor.vue";
+import Quill from "quill";
 
-richTextEditor.install = function(app) {
-  app.component(richTextEditor.name, richTextEditor)
-}
+richTextEditor.install = function (app) {
+  app.component(richTextEditor.name, richTextEditor);
+};
 
-const VueQuillEditor = { richTextEditor }
+const VueQuillEditor = { Quill, richTextEditor };
 
-export default { VueQuillEditor };
+export default VueQuillEditor;
+export { Quill, VueQuillEditor };
