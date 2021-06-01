35f410c45a974a5e34bb97254a253a65548032ec
7uvss <msh991007@gmail.com>
2021-06-01 17:41:34 +0800

    feat: init commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,4 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'lib',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'vue3-quill-editor'
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      strictDeprecations: true,
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
 })
