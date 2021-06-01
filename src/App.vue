0e9fcce5087e9dd78c0488a0273b6bc76abe8771
7uvss <msh991007@gmail.com>
2021-06-01 19:39:32 +0800

    feat: static page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="ql-editor" v-html="rawHTML"></div>
-  <richTextEditor :toolBarConfig="toolBarConfig" v-model="rawHTML" />
+  <div class="background">
+    <div class="title">raw HTML:</div>
+    <div>{{ rawHTML }}</div>
+    <div class="title">HTML preview:</div>
+    <div class="ql-editor content" v-html="rawHTML"></div>
+    <div class="title">Editor:</div>
+    <richTextEditor :toolBarConfig="toolBarConfig" v-model="rawHTML" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,11 +19,28 @@ export default defineComponent({
     richTextEditor,
   },
   setup() {
-    const toolBarConfig = [[{ header: [1, 2, 3, false] }], [{ color: [] }, { background: [] }], ['bold', 'italic', 'underline', 'strike'], [{ align: [] }], ['clean']]
+    const toolBarConfig = [
+      [{ header: [1, 2, 3, false] }],
+      [{ color: [] }, { background: [] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ align: [] }],
+      ["clean"],
+    ];
     const rawHTML = ref("");
-    return { rawHTML,toolBarConfig };
+    return { rawHTML, toolBarConfig };
   },
 });
 </script>
 
-<style></style>
+<style>
+.content {
+  border: 1px solid #ccc;
+}
+.title {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.background {
+  padding: 40px;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/vue3-quill-editor/'
-})
+  base: "/vue3-quill-editor/",
+  build: {
+    outDir: "./docs",
+  },
+});
